e47f9e7d990712b8f0b0ee45de480e44ea011327
Maxime Haselbauer <renn0xtek9@laposte.net>
2022-06-02 20:00:00 +0200

    Refactor snippetconverter.py after formatting hook have been introduced

--- snippetconverter.py
+++ snippetconverter.py
@@ -1,227 +1,194 @@
 #!/usr/bin/python3
-import sys, getopt
-from Languages import getLanguageIdentifierforVSCode,InduceLanguageFromFileName,Languages
-class bcolors:
-	NC='\033[0m'
-	Bold='\033[1m'
-	Underlined='\033[4m'
-	Blink='\033[5m'
-	Inverted='\033[7m'
-	Hidden='\033[8m'
-	Black='\033[1;30m'
-	Red='\033[1;31m'
-	Green='\033[1;32m'
-	Yellow='\033[1;33m'
-	Blue='\033[1;34m'
-	Purple='\033[1;35m'
-	Cyan='\033[1;36m'
-	LightGray='\033[1;37m'
-	DarkGray='\033[1;30m'
-	LightRed='\033[1;31m'
-	LightGreen='\033[1;32m'
-	LightYellow='\033[1;93m'
-	LightBlue='\033[1;34m'
-	LightPurple='\033[35m'
-	LightCyan='\033[1;36m'
+"""Convert snippet from Kate/KDevelop to VSCode and vice versa """
+import sys
+import getopt
+import re
+import xml.etree.ElementTree as ET
+import json
+import os.path
+from enum import Enum
+from typing import TypedDict
+from Languages import (
+    getLanguageIdentifierforVSCode,
+    InduceLanguageFromFileName,
+)
 
-	White='\033[1;97m'
-	BckgrDefault='\033[49m'
-	BckgrBlack='\033[40m'
-	BckgrRed='\033[41m'
-	BckgrGreen='\033[42m'
-	BckgrYellow='\033[43m'
-	BckgrBlue='\033[44m'
-	BckgrPurple='\033[45m'
-	BckgrCyan='\033[46m'
-	BckgrLightGray='\033[47m'
-	BckgrDarkGray='\033[100m'
-	BckgrLightRed='\033[101m'
-	BckgrLightGreen='\033[102m'
-	BckgrLightYellow='\033[103m'
-	BckgrLightBlue='\033[104m'
-	BckgrLightPurple='\033[105m'
-	BckgrLightCyan='\033[106m'
-	BckgrWhite='\033[107m'	
-	#Typical format
-	Achtung=LightRed+Bold+Blink
-	Error=LightRed+Bold
+
+class IDE(Enum):
+    """Type of IDE"""
+
+    UNKNOWN = 1
+    KATE = 2
+    VSCODE = 3
 
 
 def usage():
-	print(bcolors.LightRed +sys.argv[0] + bcolors.LightPurple+ '[-h -v --errocode -i --input -o --ouput]' +bcolors.NC)
-	print(bcolors.LightGreen +"\tWhere:")
-	print(bcolors.LightPurple +"\t-i|--input"+bcolors.LightCyan+"\tinput file")
-	print(bcolors.LightPurple +"\t-o|--ouput"+bcolors.LightCyan+"\toutput file")
-	print(bcolors.LightGreen +"\n\n\tDescription:")
-	print(bcolors.LightCyan +"\tConvert snippets from one IDE to another")	
-	print(bcolors.LightGreen +"\n\n\tExample of use:")
-	print(bcolors.LightRed +"\t"+sys.argv[0] + bcolors.LightPurple+' --input '+bcolors.LightCyan+"$HOME/.local/share/ktexteditor_snippets/CMake.xml"+ bcolors.LightPurple+' --ouput '+bcolors.LightCyan+"$HOME/.config/Code/User/snippets/cmake.json")       
-	print(bcolors.NC)
-	
-def errorlist():
-	print(bcolors.Red+"--------------------------------------------------------")
-	print("EXIT CODE       |MEANING")
-	print("--------------------------------------------------------")
-	print("0               |Success")
-	print("1               |Error when parsing argument")
-	print("255             |Exit returning information (help, version, list of error codes etc)"+bcolors.NC)
-
-def CheckAndQuitUponFolderMissing(folderlist,errorcode):
-	for folder in folderlist:
-		if (not os.path.isdir(folder)):
-			print(bcolors.LightRed+"Exit error code "+str(errorcode)+": folder "+folder+" does not exist"+bcolors.NC)
-			sys.exit(errorcode)
+    """Tells user how to use this module"""
+    print(
+        "--input $HOME/.local/share/ktexteditor_snippets/CMake.xm --output $HOME/.config/Code/User/snippets/cmake.json"
+    )
 
 
-from enum import Enum
-class IDE(Enum):
-	UNKNOWN= 1
-	KATE = 2
-	VSCODE = 3
-	
-
-def InduceIDEFromFile(filename):
-	if "ktexteditor" in filename or "kdevelop" in filename:
-		return IDE.KATE 
-	if "Code/User" in filename:		#TODO implement some better ways .(document what other IDE have for name, to ensure we won't have double match if using only *.json)
-		return IDE.VSCODE 
-	return IDE.UNKNOWN
-	
+def induce_ide_from_file(filename):
+    """Induce which IDE was used to create the file based on its name"""
+    if "ktexteditor" in filename or "kdevelop" in filename:
+        return IDE.KATE
+    # TODO implement some better ways .(document what other IDE have for name, to ensure we won't have double match if using only *.json) pylint: disable=W0511
+    if "Code/User" in filename:
+        return IDE.VSCODE
+    return IDE.UNKNOWN
 
 
-"""@class Variable 
-@brief This describe a variable in a snipppet.
-@member name the name of the varaibles withoud specific extras like ( { $ etc e.g. foobar 
-@member original text the original full text e.g. ${foobar}
-"""
-class Variable:
-	def __init__(self,name,originaltext):
-		self.name=name 
-		self.originaltext=originaltext 
+class Variable(TypedDict):
+    """@brief This describe a variable in a snipppet.
+    @member name the name of the varaibles withoud specific extras like ( { $ etc e.g. foobar
+    @member original text the original full text e.g. ${foobar}
+    """
 
-"""@class Snippet 
-@brief This describe a snippet 
-@member name the name of the snippet (this is what tyou gonna type in the IDE to match the snippet)
-@member content the content of the snippet 
-@member variables the list of variables contained in this snippet. They will have to be formatted in the output IDE format when creating a new snippet file!
-"""	
-class Snippet: 
-	def __init__(self,name,content,variables):
-		self.name=name 
-		self.content=content
-		self.variables=variables
-		
-	"""@function toVSCode()
-	@brief print the snippet into Visual Studio Code format
-	@param language the language (c++, python, javascript, brainfuck... )relevant for this snippet 
-	"""
-	def toVSCode(self,language):
-		contentlist=self.content.split('\n')
-		#Now modify the content regarding variables to ensure are correcty formatted !
-		#See https://code.visualstudio.com/docs/editor/userdefinedsnippets
-		for i in range(0,len(contentlist)):
-			for v in self.variables:
-				if v.originaltext in contentlist[i]:
-					contentlist[i].replace(v.originaltext,str("{"+v.name+"}"))
-		return dict({"scope":getLanguageIdentifierforVSCode(language),"prefix":self.name,"body":contentlist,"description":""})
+    name: str
+    originaltext: int
 
 
-"""@function getVariableListsFromKateSnippet(content)
-@brief this extract all the vaiables that exist inside a Kate Snipppet 
-@param content this is the content (array of lines) of the snippet 
-"""
-def getVariableListsFromKateSnippet(content):
-	import re
-	variables=list()
-	for c in content:
-		vars=re.findall(r'\$\{\w+\}',c)
-		for v in vars:
-			name=re.sub(r'\$\{','',v)
-			name=re.sub(r'\}','',name)
-			if not name in [var.name for var in variables]:
-				variables.append(Variable(name,v))
-	return variables
+class Snippet:
+    # pylint: disable=R0903
+    """@brief This describe a snippet
+    @member name the name of the snippet (this is what tyou gonna type in the IDE to match the snippet)
+    @member content the content of the snippet
+    @member variables the list of variables contained in this snippet. They will have to be formatted in the output IDE format when creating a new snippet file!
+    """
 
-"""ConvertKateToVSCode
-@brief Convert a snippet from a Kate/KDevelop snippet format to a visual studio code 
-@param katensippet. URL of the Kate snippet file 
-@param vscodesnippet. URL of the Visual Studio Code snippet file
-"""
-def ConvertKateToVScode(katensippet,vscodesnippet):
-	snippets=list()
-	import xml.etree.ElementTree as ET
-	import json 
-	#First we parse the Kate xml snippet to get a list of snippet that written in the file 
-	tree = ET.parse(katensippet)
-	root = tree.getroot()
-	for items in root.findall("item"):
-		try: 
-			match=items.find("match")
-			fillin=items.find("fillin")
-			variables=getVariableListsFromKateSnippet(fillin.text.split('\n'))
-			snippets.append(Snippet(match.text,fillin.text,variables))
-		except Exception as e:
-			print(e)		
-	#Second we induce which language it is 
-	language=InduceLanguageFromFileName(katensippet)
-	
-	outputsnippets=dict()
-	for snippet in snippets:
-		outputsnippets[snippet.name]=snippet.toVSCode(language)
-		
-	f=open(vscodesnippet,"w")
-	f.write(json.dumps(outputsnippets,indent=4,sort_keys=True))
-	
+    def __init__(self, name, content, variables):
+        self.name = name
+        self.content = content
+        self.variables = variables
+
+    def to_vs_code(self, language):
+        """
+        @brief print the snippet into Visual Studio Code format
+        @param language the language (c++, python, javascript, brainfuck... )relevant for this snippet
+        """
+        contentlist = self.content.split("\n")
+        # Now modify the content regarding variables to ensure are correcty formatted !
+        # See https://code.visualstudio.com/docs/editor/userdefinedsnippets
+        for content in enumerate(contentlist):
+            for variable in self.variables:
+                # print(variable)
+                if variable["original_text"] in content[1]:
+                    content[1].replace(
+                        variable["original_text"], str("{" + variable["name"] + "}")
+                    )
+                    # print("REPLACED !!!")
+                    # print(content[1])
+                    # print(contentlist)
+
+        return dict(
+            {
+                "scope": getLanguageIdentifierforVSCode(language),
+                "prefix": self.name,
+                "body": contentlist,
+                "description": "",
+            }
+        )
+
+
+def get_variable_lists_from_kate_snippet(content):
+    """@brief this extract all the vaiables that exist inside a Kate Snipppet
+    @param content this is the content (array of lines) of the snippet
+    """
+    variables = list()
+    for line in content:
+        for original_text in re.findall(r"\$\{[\w|\s]+\}", line):
+            name = re.sub(r"\$\{", "", original_text)
+            name = re.sub(r"\}", "", name)
+            name = re.sub(r" ", "_", name)
+            if not name in [var["name"] for var in variables]:
+                variable = Variable({"name": name, "original_text": original_text})
+                variables.append(variable)
+    return variables
+
+
+def convert_from_kate_to_vscode(katensippet, vscodesnippet):
+    """
+    @brief Convert a snippet from a Kate/KDevelop snippet format to a visual studio code
+    @param katensippet. URL of the Kate snippet file
+    @param vscodesnippet. URL of the Visual Studio Code snippet file
+    """
+    snippets = list()
+
+    # First we parse the Kate xml snippet to get a list of snippet that written in the file
+    tree = ET.parse(katensippet)
+    root = tree.getroot()
+    for items in root.findall("item"):
+        match = items.find("match")
+        fillin = items.find("fillin")
+        variables = get_variable_lists_from_kate_snippet(fillin.text.split("\n"))
+        snippets.append(Snippet(match.text, fillin.text, variables))
+    # Second we induce which language it is
+    language = InduceLanguageFromFileName(katensippet)
+
+    outputsnippets = dict()
+    for snippet in snippets:
+        outputsnippets[snippet.name] = snippet.to_vs_code(language)
+
+    vscode_snippetfile = open(vscodesnippet, "w")
+    vscode_snippetfile.write(json.dumps(outputsnippets, indent=4, sort_keys=True))
+
+
+def convert_vscode_to_kate(vscodesnippet, katesnippet):
+    # pylint: disable=W0613
+    """@brief Convert a Visual Studio Code snippet to a Kate Snippet
+    @param vscodesnippet URL to the visual studio snippet file
+    @param katesnippet URL to the Kate/KDevelop snippet file"""
+    print("Conversion from Visual Studio to Kate is not yet implemented")
+    sys.exit(2)
+
 
 def main(argv):
-	inputfile = ''
-	outputfile = ''
-	try:
-		opts, args = getopt.getopt(argv,"hi:o:",["errorcode","input=","ouput="])
-		
-	except getopt.GetoptError:
-		usage
-		sys.exit(1)
-		
-	for opt, arg in opts:
-		if opt == '-h':
-			usage()
-			sys.exit()
-			
-		if opt == '--errorcode' :
-			errorlist()
-			sys.exit()
-			
-		elif opt in ("-i", "--input"):
-			inputfile = arg
-			
-		elif opt in ("-o", "--ouput"):
-			outputfile = arg
-	#Write the code below, bare in minde functions must be forwarde declared
+    """module main"""
+    inputfile = ""
+    outputfile = ""
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["errorcode", "input", "ouput"])
 
-	if len(inputfile)==0 or len(outputfile)==0:
-		usage() 
-		sys.exit(1)
+    except getopt.GetoptError:
+        usage()
+        sys.exit(1)
 
-	
-	#Quit if inputfile does not exist
-	import os.path
-	if (not os.path.isfile(inputfile)):
-		raise FileNotFoundError(str("File: {} does not exist".format(inputfile)))
+    for opt, arg in opts:
+        if opt == "-h":
+            usage()
+            sys.exit()
 
-	ide_in=InduceIDEFromFile(inputfile)
-	ide_out=InduceIDEFromFile(outputfile)
-	if ide_in==IDE.UNKNOWN :
-		raise Exception(str("Could not find which IDE the file {} comes from.".format(inputfile)))
-	if ide_out==IDE.UNKNOWN :
-		raise Exception(str("Could not find which IDE the file {} comes from.".format(outputfile)))
-	
+        elif opt in ("-i", "--input"):
+            inputfile = arg
 
-	if InduceIDEFromFile(inputfile)==IDE.KATE and InduceIDEFromFile(outputfile)==IDE.VSCODE :
-		ConvertKateToVScode(inputfile,outputfile)
+        elif opt in ("-o", "--ouput"):
+            outputfile = arg
+    # Write the code below, bare in minde functions must be forwarde declared
+
+    if len(inputfile) == 0 or len(outputfile) == 0:
+        usage()
+        sys.exit(1)
+
+    if not os.path.isfile(inputfile):
+        raise FileNotFoundError(str("File: {} does not exist".format(inputfile)))
+
+    ide_in = induce_ide_from_file(inputfile)
+    ide_out = induce_ide_from_file(outputfile)
+    if ide_in == IDE.UNKNOWN:
+        raise Exception(
+            str("Could not find which IDE the file {} comes from.".format(inputfile))
+        )
+    if ide_out == IDE.UNKNOWN:
+        raise Exception(
+            str("Could not find which IDE the file {} comes from.".format(outputfile))
+        )
+
+    if ide_in == IDE.KATE and ide_out == IDE.VSCODE:
+        convert_from_kate_to_vscode(inputfile, outputfile)
+    if ide_in == IDE.VSCODE and ide_out == IDE.KATE:
+        convert_vscode_to_kate(inputfile, outputfile)
 
 
 if __name__ == "__main__":
-        main(sys.argv[1:])
-
-
+    main(sys.argv[1:])
