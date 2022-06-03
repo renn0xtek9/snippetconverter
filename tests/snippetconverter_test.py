0fc03fdb2b9984730d6103e1c3661ae34c66fd62
Maxime Haselbauer <renn0xtek9@laposte.net>
2022-06-03 13:04:18 +0200

    Add unit tests for snippertconverter.py

---
+++ tests/snippetconverter_test.py
@@ -0,0 +1,32 @@
+"""unit tests of snippetconverter_test.py"""
+from snippetconverter.snippetconverter import (
+    get_variable_lists_from_kate_snippet,
+    Variable,
+    induce_ide_from_file,
+    IDE,
+)
+
+
+def test_get_varaible_lists_from_kate_snippet():
+    """
+    @brief test the get_variable_lists_from_kate_snippet
+    """
+    content = ["blalba\\%27!@#&^@${foo}):", "${bar}"]
+    expected = [
+        Variable({"name": "foo", "original_text": "${foo}"}),
+        Variable({"name": "bar", "original_text": "${bar}"}),
+    ]
+    assert expected == get_variable_lists_from_kate_snippet(content)
+
+
+def test_induce_ide_from_file():
+    """
+    @brief test the induction of ide from the filename
+    """
+    # pylint: disable=W1401
+    kate_snippet_file_path = (
+        "/home/foo/.local/share/ktexteditor_snippets/data/CMake\ snippets.xml"
+    )
+    vscode_snippet_file_path = "/home/foo/.config/Code/User/snippets/cmake.json"
+    assert IDE.VSCODE == induce_ide_from_file(vscode_snippet_file_path)
+    assert IDE.KATE == induce_ide_from_file(kate_snippet_file_path)
