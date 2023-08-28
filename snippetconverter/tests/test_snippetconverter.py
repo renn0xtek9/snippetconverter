770ee714bb1eb709bd12847a385665e17d9223ea
Maxime Haselbauer <renn0xtek9@laposte.net>
2023-08-28 23:28:47 +0200

    Fix problem with the argv in v0.3

--- snippetconverter/tests/test_snippetconverter.py
+++ snippetconverter/tests/test_snippetconverter.py
@@ -1,18 +1,18 @@
 """unit tests of snippetconverter_test.py"""
+import sys
+from unittest.mock import MagicMock, patch
+
 import pytest
-import pathlib
-from unittest.mock import patch
-from unittest.mock import MagicMock
-from pathlib import Path
+
 from snippetconverter.snippetconverter import (
     IDE,
     Variable,
-    get_variable_lists_from_kate_snippet,
-    induce_ide_from_file,
-    usage,
     convert_from_kate_to_vscode,
     convert_vscode_to_kate,
-    main
+    get_variable_lists_from_kate_snippet,
+    induce_ide_from_file,
+    main,
+    usage,
 )
 
 
@@ -43,16 +43,14 @@ def test_induce_ide_from_file():
     assert IDE.KATE == induce_ide_from_file(kate_snippet_file_path)
     assert IDE.UNKNOWN == induce_ide_from_file(unknown_snippet_file_path)
 
+
 def test_usage_does_no_throw():
     """
     @brief test that usage does not throw"""
     usage()
 
 
-
-
-
-DUMMY_XML_CONTENT="""<snippets authors=\"max\" license=\"BSD\" name=\"C snippets\" filetypes=\".c\" namespace=\"\">
+DUMMY_XML_CONTENT = """<snippets authors=\"max\" license=\"BSD\" name=\"C snippets\" filetypes=\".c\" namespace=\"\">
 <script>function fooFunc() { return document.fileName(); }
 </script>
 <item>
@@ -63,7 +61,7 @@ DUMMY_XML_CONTENT="""<snippets authors=\"max\" license=\"BSD\" name=\"C snippets
 </snippets>
 """
 
-EXPECT_C_JSON_CONTENT="""{
+EXPECT_C_JSON_CONTENT = """{
     "dolphin_service_menu": {
         "body": [
             "int ${a};",
@@ -75,66 +73,135 @@ EXPECT_C_JSON_CONTENT="""{
     }
 }"""
 
+
 def create_temporary_c_snippet_for_kate_and_vscode(tmp_path):
-    kate_snippet_folder=tmp_path/ "kdevelop" / "data"
-    kate_snippet_folder.mkdir( parents=True)
+    """create temporary c snippet for test purposes"""
+    kate_snippet_folder = tmp_path / "kdevelop" / "data"
+    kate_snippet_folder.mkdir(parents=True)
     kate_snippet_c = kate_snippet_folder / "C snippets.xml"
     kate_snippet_c.write_text(DUMMY_XML_CONTENT)
 
-    code_snippet_folder=tmp_path/ "Code" / "User"/"snippets"
-    code_snippet_folder.mkdir( parents=True)
+    code_snippet_folder = tmp_path / "Code" / "User" / "snippets"
+    code_snippet_folder.mkdir(parents=True)
     code_snippet_c = code_snippet_folder / "c.json"
 
-    return [kate_snippet_c,code_snippet_c]
+    return [kate_snippet_c, code_snippet_c]
+
 
 def test_convert_from_kate_to_vscode(tmp_path):
-    [kate_snippet_c,code_snippet_c]=create_temporary_c_snippet_for_kate_and_vscode(tmp_path)
+    """test conversion from kate to vscode"""
+    [kate_snippet_c, code_snippet_c] = create_temporary_c_snippet_for_kate_and_vscode(
+        tmp_path
+    )
 
-    convert_from_kate_to_vscode(kate_snippet_c,code_snippet_c)
+    convert_from_kate_to_vscode(kate_snippet_c, code_snippet_c)
     assert code_snippet_c.read_text() == EXPECT_C_JSON_CONTENT
 
 
 def test_convert_vscode_to_kate(tmp_path):
-    [kate_snippet_c,code_snippet_c]=create_temporary_c_snippet_for_kate_and_vscode(tmp_path)
+    """test conversion from vscode to kate"""
+    [kate_snippet_c, code_snippet_c] = create_temporary_c_snippet_for_kate_and_vscode(
+        tmp_path
+    )
     with pytest.raises(NotImplementedError):
-        convert_vscode_to_kate(code_snippet_c,kate_snippet_c)
+        convert_vscode_to_kate(code_snippet_c, kate_snippet_c)
 
-    
 
 def test_main_throws_if_input_file_does_not_exit():
-    argv=['./__main__.py', '-i', 'inexistant_file.xml', '-o', '/home/foo/.config/Code/User/snippets/cmake.json']
+    """test that main throws when no input file exists"""
+    sys.argv = [
+        "./__main__.py",
+        "-i",
+        "inexistent_file.xml",
+        "-o",
+        "/home/foo/.config/Code/User/snippets/cmake.json",
+    ]
     with pytest.raises(FileNotFoundError):
-        main(argv)
+        main()
 
 
-convert_from_kate_to_vscode_mock=MagicMock()
-@patch('snippetconverter.snippetconverter.convert_from_kate_to_vscode')
-def test_main_convert_correctly_when_input_is_kate_snippet(convert_from_kate_to_vscode_mock,tmp_path):
-    [kate_snippet_c,code_snippet_c]=create_temporary_c_snippet_for_kate_and_vscode(tmp_path)
-    argv=['./__main__.py', '-i', str(kate_snippet_c.absolute()), '-o', str(code_snippet_c.absolute())]
-    main(argv)
+convert_from_kate_to_vscode_mock = MagicMock()
+
+
+@patch("snippetconverter.snippetconverter.convert_from_kate_to_vscode")
+def test_main_convert_correctly_when_input_is_kate_snippet(
+    # pylint: disable=W0621
+    convert_from_kate_to_vscode_mock,
+    tmp_path,
+):
+    """test main with kate snippet file as input"""
+    [kate_snippet_c, code_snippet_c] = create_temporary_c_snippet_for_kate_and_vscode(
+        tmp_path
+    )
+    sys.argv = [
+        "./__main__.py",
+        "-i",
+        str(kate_snippet_c.absolute()),
+        "-o",
+        str(code_snippet_c.absolute()),
+    ]
+    main()
     assert convert_from_kate_to_vscode_mock.called
 
-convert_vscode_to_kate_mock=MagicMock()
-@patch('snippetconverter.snippetconverter.convert_vscode_to_kate')
-def test_main_convert_correctly_when_input_is_code_snippet(convert_vscode_to_kate_mock,tmp_path):
-    [kate_snippet_c,code_snippet_c]=create_temporary_c_snippet_for_kate_and_vscode(tmp_path)
+
+convert_vscode_to_kate_mock = MagicMock()
+
+
+@patch("snippetconverter.snippetconverter.convert_vscode_to_kate")
+def test_main_convert_correctly_when_input_is_code_snippet(
+    # pylint: disable=W0621
+    convert_vscode_to_kate_mock,
+    tmp_path,
+):
+    """test main with vscode snippet file as input"""
+    [kate_snippet_c, code_snippet_c] = create_temporary_c_snippet_for_kate_and_vscode(
+        tmp_path
+    )
     code_snippet_c.write_text(EXPECT_C_JSON_CONTENT)
-    argv=['./__main__.py', '-i', str(code_snippet_c.absolute()), '-o', str(kate_snippet_c.absolute())]
-    main(argv)
+    sys.argv = [
+        "./__main__.py",
+        "-i",
+        str(code_snippet_c.absolute()),
+        "-o",
+        str(kate_snippet_c.absolute()),
+    ]
+    main()
     assert convert_vscode_to_kate_mock.called
-    
-def test_main_input_IDE_unknown(tmp_path):
-    [kate_snippet_c,code_snippet_c]=create_temporary_c_snippet_for_kate_and_vscode(tmp_path)
-    some_file=tmp_path / "foobar.yaml"
+
+
+def test_main_input_ide_unknown(tmp_path):
+    """test main when input IDE is unknown"""
+    # pylint: disable=W0612
+    [kate_snippet_c, code_snippet_c] = create_temporary_c_snippet_for_kate_and_vscode(
+        tmp_path
+    )
+    some_file = tmp_path / "foobar.yaml"
     some_file.write_text(DUMMY_XML_CONTENT)
-    argv=['./__main__.py', '-i', str(some_file.absolute()), '-o', str(kate_snippet_c.absolute())]
+    sys.argv = [
+        "./__main__.py",
+        "-i",
+        str(some_file.absolute()),
+        "-o",
+        str(kate_snippet_c.absolute()),
+    ]
     with pytest.raises(RuntimeError):
-        main(argv)
-    
-def test_main_output_IDE_unknown(tmp_path):
-    [kate_snippet_c,code_snippet_c]=create_temporary_c_snippet_for_kate_and_vscode(tmp_path)
+        main()
+
+
+def test_main_output_ide_unknown(tmp_path):
+    """test main when output IDE is unknown"""
+    # pylint: disable=W0612
+    [kate_snippet_c, code_snippet_c] = create_temporary_c_snippet_for_kate_and_vscode(
+        tmp_path
+    )
+
     code_snippet_c.write_text(EXPECT_C_JSON_CONTENT)
-    argv=['./__main__.py', '-i', str(code_snippet_c.absolute()), '-o', "/home/foo"]
+    sys.argv = [
+        "./__main__.py",
+        "-i",
+        str(code_snippet_c.absolute()),
+        "-o",
+        "/home/foo",
+    ]
     with pytest.raises(RuntimeError):
-        main(argv)
+        main()
